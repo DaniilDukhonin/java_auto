36c7494542a97b8eeac137ba1d0d6493332ca08c
ddukhoni <danigeist@mail.ru>
2020-10-02 11:58:24 +0300

    задание 3 дорботал

--- sandbox/src/test/java/ru/rstqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/rstqa/pft/sandbox/PointTest.java
@@ -8,9 +8,9 @@ public class PointTest {
 
   @Test
   public void testDistance() {
-    Point p1 = new Point(3,5);
-    Point p2 = new Point(11,0);
-    Assert.assertEquals(p1.distance(), p2.distance(), 0);
+    Point p1 = new Point(3.0,5.0);
+    Point p2 = new Point(11.0,0.0);
+    Assert.assertEquals(p1.distance(p2), 0.0);
 
 
   }
