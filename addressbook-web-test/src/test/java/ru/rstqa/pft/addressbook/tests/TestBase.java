14d49ee583b0de7d7a55cb102e2286273544981a
ddukhoni <danigeist@mail.ru>
2020-10-22 22:59:59 +0300

    задание 7 - запуск в разных браузерах

--- addressbook-web-test/src/test/java/ru/rstqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/rstqa/pft/addressbook/tests/TestBase.java
@@ -3,10 +3,18 @@ package ru.rstqa.pft.addressbook.tests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.rstqa.pft.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
